d72bcf6139068ab9799376e00fba0e386cc191e2
John Doe <3455634289@qq.com>
2019-03-13 09:04:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/lzl/activity/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.administrator.lzl.activity;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.administrator.lzl.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView tv_main_title;
+    private TextView tv_back,tv_register,tv_find_psw;
+    private Button btn_login;
+    private String userName,psw,spPsw;
+    private EditText et_user_name,et_psw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        imit();
+    }
+
+    @SuppressLint("WrongViewCast")
+    private void imit() {
+        tv_main_title=(TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("登录");
+        tv_back=(TextView) findViewById(R.id.tv_back);
+        tv_register=(TextView) findViewById(R.id.tv_register);
+        tv_find_psw=(TextView) findViewById(R.id.tv_find_psw);
+        btn_login=(Button) findViewById(R.id.btn_login);
+    }
+}
